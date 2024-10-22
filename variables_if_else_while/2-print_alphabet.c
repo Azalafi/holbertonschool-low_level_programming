cc9bec8f56d8c54e6956461ac0879ea0c4c084c1
Azalafi <9940@holbertonstudents.com>
2024-10-22 14:15:32 +0000

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - principal function
+*
+*Return: 0 if the 0 is the  answer then  success
+*/
+int main(void)
+{
+char alf = 'a';
+char abeto = 'z';
+
+while (alf <= abeto)
+{
+putchar(alf);
+alf++;
+}
+putchar ('\n');
+return (0);
+}
