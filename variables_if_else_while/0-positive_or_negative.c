0735b3840bdb0d5431bac8993fd953ba6e410923
Azalafi <9940@holbertonstudents.com>
2024-10-22 13:44:55 +0000

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
-   
+    int n;
+
+    srand(time(0)); // Initialise le générateur de nombres aléatoires
+    n = rand() % 201 - 100; // Génère un nombre entre -100 et 100
+
+    printf("%d ", n);
+    if (n > 0)
+    {
+        printf("est positif.\n");
+    }
+    else if (n == 0)
+    {
+        printf("est nul.\n");
+    }
+    else
+    {
+        printf("est négatif.\n");
+    }
+
     return 0;
 }
 
