8e04815c6c05bc656661a7e4d29790cdb8f43849
Azalafi <9940@holbertonstudents.com>
2024-10-22 12:17:32 +0000

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- * Description: Prints a quote using the puts function
- * Return: Always 0 (Success)
- */
+int main(void)
+{
+   
+    return 0;
+}
+
