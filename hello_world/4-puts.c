3fadf9c945d20feea8c1d0288af425f8c6a07030
Azalafi <9940@holbertonstudents.com>
2024-10-22 08:58:14 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc -o puts ("Programming is like building a multilingual puzzle")
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a quote using the puts function
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
