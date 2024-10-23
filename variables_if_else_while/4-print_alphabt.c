f3a25417ffcf8a8c288ad088e486586d5557246b
Azalafi <9940@holbertonstudents.com>
2024-10-23 11:55:38 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Affiche l'alphabet sans q et e
+ *
+ * Return: Toujours 0 (Succès)
+ */
+int main(void)
+{
+    char lettre;
+
+    for (lettre = 'a'; lettre <= 'z'; lettre++)
+    {
+        if (lettre != 'e' && lettre != 'q')
+            putchar(lettre);
+    }
+    putchar('\n');
+
+    return (0);
+}
