06544c056b30329d2be9453487d142bc0ed4ad07
Azalafi <9940@holbertonstudents.com>
2024-10-24 14:02:46 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
-* main - putchar
-*
-* return: 0
-*/
-
+ * main - Affiche "_putchar" suivi d'une nouvelle ligne
+ *
+ * Return: Toujours 0 (Succès)
+ */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
 }
