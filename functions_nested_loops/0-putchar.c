fe289cff0ebc14cad35e7dea35be4f35cba5be6e
Azalafi <9940@holbertonstudents.com>
2024-10-24 13:35:14 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
-int _putchar(char c);
+/**
+* main - putchar
+* 
+* return: 0
+*/
 
 int main(void)
-
 {
 _putchar('_');
 _putchar('p');
@@ -14,5 +17,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-    return (0);
+return (0);
 }
