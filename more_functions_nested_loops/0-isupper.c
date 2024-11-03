d6680d884b9700d876fc0bf752c062c14a93e5ca
root <Azalafi>
2024-11-03 22:17:14 +0000

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,14 @@
+#include "main.h"
 /**
- * _isupper - vérifie si le caractère est en majuscule
- * @c: le caractère à vérifier
- * Return: 1 si c est une majuscule, 0 sinon
- */
+*_isupper - function that verifies if a  character is uppercase or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
+
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-        return (1);
-    else
-        return (0);
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
+
+return (0);
 }
